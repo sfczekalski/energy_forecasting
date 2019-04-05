c4371b8be1c501aa91a457c69be6b18147e10e5d
sfczekalski <sfczekalski@gmail.com>
2019-04-05 20:22:32 +0200

    Start and end rows in dataframe parametrized. Preprocessed csv file ready.

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 
 #zakres wierszy: 2929 - 20471 (2016.01.01 - 2017.12.31)
+start_row = 2929
+end_row = 20472
+
 initial_df = pd.read_csv("../data/raw/odczyty_archh_urzadz_5004.csv",
                          sep=";",
                          usecols=["Energia", "T zewn.", "V wiatr",
@@ -15,7 +18,7 @@ initial_df = pd.read_csv("../data/raw/odczyty_archh_urzadz_5004.csv",
                              "Typ dnia": "int32",
                              "Pora roku": "int32"
                          },
-                         decimal=",").iloc[2929:20472]
+                         decimal=",").iloc[start_row:end_row]
 
 initial_df["Energia"] = 0
 
@@ -25,12 +28,13 @@ for filename in all_files:
                        sep=';',
                        usecols=["Energia"],
                        skiprows=lambda x: x in [1, 2],
-                       decimal=",").iloc[2929:20472]
+                       decimal=",").iloc[start_row:end_row]
 
     initial_df["Energia"] += file["Energia"]
 
 initial_df.index = np.arange(1, len(initial_df)+1)
 
 print(initial_df.head())
+initial_df.to_csv("data.csv")
 
 
